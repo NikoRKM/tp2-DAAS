4890cabda1644360584e0c47b4e5ae12ade6ff54
Nicolas Velasquez <nicolasyago9@gmail.com>
2026-09-22 18:32:57 -0300

    add client repository test and client service test

--- src/main/java/ar/edu/unju/fi/tp2/models/Cliente.java
+++ src/main/java/ar/edu/unju/fi/tp2/models/Cliente.java
@@ -14,6 +14,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -24,6 +25,7 @@ import lombok.ToString;
 @ToString(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "cliente")
 public class Cliente extends Auditable {
